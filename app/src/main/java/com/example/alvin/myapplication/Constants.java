06122d345ed4b160a4576d2f2efcab486b51771b
muriungialvin <alvin.muriungi@strathmore.edu>
2018-08-09 17:11:43 +0300

    combined updated code 2

---
+++ app/src/main/java/com/example/alvin/myapplication/Constants.java
@@ -0,0 +1,7 @@
+package com.example.alvin.myapplication;
+
+public class Constants {
+    public static final String UPLOAD_URL = "http://192.168.94.1/AndroidImageUpload/upload.php";
+    public static final String IMAGES_URL = "http://192.168.94.1/AndroidImageUpload/getImages.php";
+
+}
